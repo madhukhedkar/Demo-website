94a6664d8eb2f7b41b9cf4013bf4340b522c6662
“madhukhedkar” <khedkar363@gmail.com>
2022-03-30 14:43:04 +0530

    Adavanced method of string

--- javascript.js
+++ javascript.js
@@ -5,6 +5,11 @@ const lastName = 'Khedkar';
 const birthYear = 1992;
 const year = 2022;
 
-const madhu = "I'm " + firstName +" " + lastName + ', a ' + (year - birthYear) + ' old tearcher!';
+// const madhu = "I'm " + firstName +" " + lastName + ', a ' + (year - birthYear) + ' old tearcher!';
 
+// console.log (madhu);
+
+//Adavanced method
+
+const madhu = `I'm ${firstName}, a ${year - birthYear} old teacher!`;
 console.log (madhu);
