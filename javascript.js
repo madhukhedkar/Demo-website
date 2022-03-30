d5d90900299798a7b1b2724b02fd720ae6d9b98a
“madhukhedkar” <khedkar363@gmail.com>
2022-03-30 14:34:06 +0530

    Learning string

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
-const madhu = 1;
+// string and Templates
+
+const firstName = 'Madhu';
+const lastName = 'Khedkar';
+const birthYear = 1992;
+const year = 2022;
+
+const madhu = "I'm " + firstName +" " + lastName + ', a ' + (year - birthYear) + ' old tearcher!';
 
 console.log (madhu);
