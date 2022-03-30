04a7e858647e3316696834f9f73143ba1a90e23b
“madhukhedkar” <khedkar363@gmail.com>
2022-03-30 14:59:03 +0530

    Learn if else cnditions

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 // string and Templates
 
-const firstName = 'Madhu';
-const lastName = 'Khedkar';
-const birthYear = 1992;
-const year = 2022;
+// const firstName = 'Madhu';
+// const lastName = 'Khedkar';
+// const birthYear = 1992;
+// const year = 2022;
 
 // const madhu = "I'm " + firstName +" " + lastName + ', a ' + (year - birthYear) + ' old tearcher!';
 
@@ -11,5 +11,17 @@ const year = 2022;
 
 //Adavanced method
 
-const madhu = `I'm ${firstName}, a ${year - birthYear} old teacher!`;
-console.log (madhu);
+// const madhu = `I'm ${firstName}, a ${year - birthYear} old teacher!`;
+// console.log (madhu);
+
+//if else statement
+
+const age = 15
+const isOldOk = age >= 18;
+
+if (isOldOk) {
+    console.log('Madhu can get license');
+}
+else {
+    console.log('Madhu can not get license');
+}
