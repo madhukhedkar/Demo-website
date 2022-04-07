e01c75fabcf7df07b2a85e234809452d075547aa
Madhu Khedkar <khedkar363@gmail.com>
2022-04-07 13:23:58 +0530

    Learn ternary operators

--- javascript.js
+++ javascript.js
@@ -175,25 +175,20 @@
 //     console.log("Not a valid day");
 // }
 
-const language = 'arabic';
+// 
 
-switch (language){
-    case 'chinese':
-    case 'mandarin':
-        console.log("MOST number of native speakers!");
-    break;
-    case 'spanish':
-        console.log("2nd place in number of native speakers");
-    break;
-    case 'english':
-        console.log("3rd place");
-    break;
-    case 'hindi':
-        console.log("Number 4");
-    break;
-    case 'arabic':
-        console.log("5th most spoken language");
-    break;
-    default:
-        console.log("Great language too :D'");     
-}
+
+// The conditional (Ternary) Operators
+
+const age = 23;
+
+age >= 18 ? console.log("Wine 🍷") : console.log("Water💧");
+
+
+const drink = age >= 18 ? "Wine 🍷" : "Water💧";
+
+console.log(drink);
+
+const ageMy = 23;
+
+ageMy >= 18 ? console.log("can drive") : console.log("can't drive");
